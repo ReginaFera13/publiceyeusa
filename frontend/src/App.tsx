c4a834a26ded3677bb631b85a2b2b227d7af03ea
ReginaFera13 <shawnmariesmith@icloud.com>
2024-05-29 19:12:29 -0400

    signup page skeleton created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { Outlet, useNavigate, useLocation, useLoaderData } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import HomePage from "./pages/HomePage";
 import MyNavbar from './components/Navbar';
 
 function App() {
+  const [user, setUser] = useState(useLoaderData());
+  const [userProfileData, setUserProfileData] = useState([])
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      if (user) {
+        try {
+          const userProfileData = await getUserProfile(user);
+          setUserProfileData(userProfileData);
+          // console.log('User profile data loaded:', userProfileData);
+        } catch (error) {
+          console.error('Error fetching user profile data:', error);
+        }
+      }
+    };
+  
+    fetchUserProfile();
+  
+    let nullUserUrls = ["/login", "/signup"] // should redirect to profile if logged in
+    let allowNonUserUrls = ["/login", "/signup", "/", "/events"] // should allow if not logged in
+  
+      // check if current url is one that might need to redirect
+      let isNullUserUrl = nullUserUrls.includes(location.pathname)
+      let isAllowedNonUserUrl = allowNonUserUrls.includes(location.pathname);
+  
+  
+      // redirect to profile page when
+      // logged user tries to go to signup, etc
+      if(user && isNullUserUrl) {
+        navigate("/")
+      }
+  
+      // not logged in user tries to go anywhere BUT signup, login, home or events
+      // we redirect because the user needs to log in before they do anything else
+      else if (!user && !isAllowedNonUserUrl){
+        navigate("/")
+      }
+  
+    }, [user, location.pathname]);
 
   return (
   <>
-    <MyNavbar/>
-    <HomePage/>
+    <MyNavbar setUserProfileData={setUserProfileData} user={ user } setUser={ setUser }/>
+    <Outlet context={{ user, setUser, userProfileData, setUserProfileData }}/>
   </>
   )
 }
